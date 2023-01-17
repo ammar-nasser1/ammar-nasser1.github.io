e5dc9034469de47890a68ac8cc71ef27c40c7ce9
Ammar <Ammar12@gmail.com>
2023-01-17 04:47:16 +0200

    dfd

---
+++ ioo/src/views/myIo.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>hi ,i am ammar</h1>
+  <img src="../assets/1.png" alt="asdasd" />
+  <h2>sf</h2>
+</template>
+<style>
+img {
+  width: 200px;
+  height: 200px;
+  border: 5px solid black;
+}
+</style>
